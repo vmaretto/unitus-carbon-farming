335bf394c44590657c4632f4ef5c622bca4270f3
Virgilio Maretto <virgiliomaretto@MinidiVirgilio.homenet.telecomitalia.it>
2026-03-27 13:01:34 +0100

    debug: log per edit enrollment

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const upload = multer({
     }
   }
 });
-const BUILD_VERSION = '2026-03-27-v16'; // Per debug deploy
+const BUILD_VERSION = '2026-03-27-v17'; // Per debug deploy
 
 // Health check
 app.get('/api/health', (req, res) => {
